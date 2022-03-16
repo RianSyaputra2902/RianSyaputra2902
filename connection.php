65ad6d8adeff76e4bebacf4a86bd223931097613
Rian Syaputra <79958214+RianSyaputra2902@users.noreply.github.com>
2022-03-16 16:24:52 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$conn_string = "host = ec2-34-224-226-38.compute-1.amazonaws.com" . 
-  "port=5432 dbname=dcpgji8n0ra2sm" .
-  "user=loyvnysswxbfga" . 
-  "password=cb138d93ed7f63794236aa1474817cff48d48bb5887b52cbe9da039fff0b7125";
+$conn_string = "host=john.db.elephantsql.com" . 
+  "port=5432 dbname=zfblrwcz" .
+  "user=zfblrwcz" . 
+  "password=JVzibiIzivwBxbc2ISZeP9gY31ocfYXq";
 $conn = pg_connect($conn_string);
 
 if($conn) {
