ecce70071527475f930bdac41fa59babe2a14131
Rian Syaputra <79958214+RianSyaputra2902@users.noreply.github.com>
2022-03-16 16:12:52 +0700

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,8 +1,8 @@
 <?php
-$conn_string = "host=john.db.elephantsql.com " . 
-  "port=5432 dbname=bbabwdoj " .
-  "user=bbabwdoj " . 
-  "password=ymZ9MmRIoXZ_YS8YThBXkn11Zc-e56h5";
+$conn_string = "john.db.elephantsql.com " . 
+  "port=5432 dbname=zfblrwcz " .
+  "user=zfblrwcz " . 
+  "password=JVzibiIzivwBxbc2ISZeP9gY31ocfYXq";
 $conn = pg_connect($conn_string);
 
 if($conn) {
